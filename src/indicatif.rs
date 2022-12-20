0a05fbaff10096fa7f4905a7b72cafe5ace22ab2
Milo Moisson <milomoisson@gmail.com>
2022-12-20 19:23:35 +0100

    feat: move dialoguer theme to a properly named file

---
+++ src/indicatif.rs
@@ -0,0 +1,29 @@
+//! A label theme for indicatif progress bars
+
+use crate::{pretty_output, OutputLabel};
+use indicatif::ProgressStyle;
+
+/// Builds and return the theme for a progress bar
+///
+/// # Panics
+/// If the template carefully built inside our crate is wrong
+#[must_use]
+pub fn label_theme(label: OutputLabel) -> ProgressStyle {
+	let template = "[{bar:20}] {pos}/{len} {elapsed} {wide_msg}";
+	let template = pretty_output(label, template);
+
+	ProgressStyle::with_template(&template)
+		.expect("our carefully built template is apparently wrong")
+		.progress_chars("=> ")
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::OutputLabel;
+
+	#[test]
+	fn label_theme_template_is_not_broken() {
+		let _theme = label_theme(OutputLabel::Success("Not broken"));
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
-#![warn(clippy::missing_docs_in_private_items)]
+#![warn(
+	clippy::missing_docs_in_private_items,
+	clippy::unwrap_used,
+	clippy::nursery,
+	clippy::pedantic,
+	clippy::cargo
+)]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/MrNossiom/label-logger/main/logo.png")]
 #![doc = include_str!("../README.md")]
 
+#[cfg(feature = "dialoguer")]
+pub mod dialoguer;
 #[cfg(feature = "indicatif")]
 pub mod indicatif;
 mod log;
 mod macros;
-#[cfg(feature = "dialoguer")]
-mod theme;
 mod util;
 
 // Re-exports
+#[cfg(feature = "dialoguer")]
+pub use crate::dialoguer::LabelTheme;
 #[cfg(feature = "indicatif")]
 pub use crate::indicatif::label_theme;
 pub use crate::log::{pretty_output, println_label, OutputLabel};
 pub use crate::macros::*;
-#[cfg(feature = "dialoguer")]
-pub use crate::theme::LabelTheme;
 pub use console;
-#[cfg(feature = "dialoguer")]
-pub use dialoguer;
